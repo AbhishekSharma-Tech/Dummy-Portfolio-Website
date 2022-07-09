f65e95b9d27b478f7d1d69105c2beabf2fb1861a
Abhishek Sharma <76276127+AbhishekSharma-Tech@users.noreply.github.com>
2022-07-09 15:07:37 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,23 @@
+(function ($) {
+    "use strict";
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+})(jQuery);
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
